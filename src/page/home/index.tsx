534c93d63686eef2850d68cfa8c2c3ba156c5b8e
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-06 19:57:44 +0530

    Updated home component

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -4,9 +4,12 @@ import image from "../../assets/image/restaurant-hall-with-lots-table.jpg";
 
 export function Home() {
     return (
-        <section>
-            <div id="home-img" className="relative h-full" style={{background:`url(${image})`,backgroundSize:"cover",backgroundPosition:"center"}}>
-                <div id="img-cover" className="bg-gray-950 opacity-25 w-full h-full absolute top-0"/>
+        <section className="relative">
+            <div id="home-img" className="relative h-full" style={{background:`url(${image})`,backgroundSize:"cover",backgroundPosition:"center"}}/>
+            <div className="bg-gray-950 opacity-25 w-2/4 h-full absolute top-0"/>
+            <div id="home-right-side-cover" className="flex f-col w-2/4 h-full absolute top-0 right-0">
+                <div className="bg-gray-950 absolute opacity-50 w-full h-full relative"/>
+                <h1 id="home-topic" className="flex top-0 absolute bottom-0 margin-auto text-6xl text-white ">FE RESTAURANT RECEIVE YOUR TABLE EASILY</h1>
             </div>
         </section>
     )
