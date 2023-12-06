45ad2380cce5b8c4586e7ad6b6b20ee56c429c66
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-06 20:41:31 +0530

    Updated the home component to be responsive to all devices

--- src/page/home/index.tsx
+++ src/page/home/index.tsx
@@ -5,11 +5,15 @@ import image from "../../assets/image/restaurant-hall-with-lots-table.jpg";
 export function Home() {
     return (
         <section className="relative">
-            <div id="home-img" className="relative h-full" style={{background:`url(${image})`,backgroundSize:"cover",backgroundPosition:"center"}}/>
-            <div className="bg-gray-950 opacity-25 w-2/4 h-full absolute top-0"/>
-            <div id="home-right-side-cover" className="flex f-col w-2/4 h-full absolute top-0 right-0">
+            <div id="home-img" className="relative h-full"
+                 style={{background: `url(${image})`, backgroundSize: "cover", backgroundPosition: "center"}}/>
+            <div className="bg-gray-950 opacity-0 w-2/4 h-full absolute top-0 sm:opacity-25"/>
+            <div id="home-right-side-cover" className="flex f-col w-full h-full absolute top-0 right-0 sm:w-2/4">
                 <div className="bg-gray-950 absolute opacity-50 w-full h-full relative"/>
-                <h1 id="home-topic" className="flex top-0 absolute bottom-0 margin-auto text-6xl text-white ">FE RESTAURANT RECEIVE YOUR TABLE EASILY</h1>
+                <h1 id="home-topic"
+                    className="mx-10 text-center flex top-0 absolute bottom-0 margin-auto text-5xl text-white sm:mr-5 sm:text-left sm:text-5xl md:text-6xl
+                     lg:text-7xl 2xl:text-8xl">FE
+                    RESTAURANT RECEIVE YOUR TABLE EASILY</h1>
             </div>
         </section>
     )
