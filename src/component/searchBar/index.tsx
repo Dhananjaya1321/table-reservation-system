f9556f1aa32b0602fa250978486052da47f42cd5
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-07 18:40:41 +0530

    Updated the search bar component to be responsive to all devices

--- src/component/searchBar/index.tsx
+++ src/component/searchBar/index.tsx
@@ -5,10 +5,10 @@ import "./style.css";
 export function SearchBar() {
     return (
         <section id="search-bar" className="h-max w-full flex">
-            <div className="flex-wrap w-max h-28 my-5 drop-shadow-lg rounded-lg flex gap-y-4">
-                <input className="w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"date"}/>
-                <input className="w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"time"}/>
-                <Button className="w-56 h-10 rounded px-1" variant="contained">Medium</Button>
+            <div className="flex-wrap w-max sm:h-28 my-5 drop-shadow-2xl rounded-lg flex gap-y-4">
+                <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"date"}/>
+                <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"time"}/>
+                <Button className="w-40 md:w-56 h-10 rounded px-1" variant="contained">Medium</Button>
             </div>
         </section>
     )
