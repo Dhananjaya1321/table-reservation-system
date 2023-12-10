748c915bec5116b0c5c5ec29cc4d937d7f3761e6
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-10 22:37:06 +0530

    Updated

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -7,10 +7,10 @@ export function Home() {
             <div id="home-img" className="relative h-full"
                  style={{background: `url(${image})`, backgroundSize: "cover", backgroundPosition: "center"}}/>
             <div className="bg-gray-950 opacity-0 w-2/4 h-full absolute top-0 sm:opacity-25"/>
-            <div id="home-right-side-cover" className="flex f-col w-full h-full absolute top-0 right-0 sm:w-2/4">
+            <div id="home-right-side-cover" className="flex flex-col w-full h-full absolute top-0 right-0 sm:w-2/4">
                 <div className="bg-gray-950 opacity-50 w-full h-full relative"/>
                 <h1 id="home-topic"
-                    className="mx-10 text-center flex f-col top-0 absolute bottom-0 margin-auto text-5xl text-white sm:mr-5 sm:text-left sm:text-5xl md:text-6xl
+                    className="mx-10 text-center flex flex-col justify-center top-0 absolute bottom-0 margin-auto text-5xl text-white sm:mr-5 sm:text-left sm:text-5xl md:text-6xl
                      lg:text-7xl 2xl:text-8xl"><span
                     className="text-8xl sm:text-left sm:self-start lg:text-8xl 2xl:text-10xl">FE</span> RESTAURANT<br/> RECEIVE
                     YOUR TABLE EASILY
--- src/views/searchBar/index.tsx
+++ src/views/searchBar/index.tsx
@@ -4,8 +4,8 @@ import "./style.css";
 
 export function SearchBar() {
     return (
-        <section id="search-bar" className="h-max w-full flex">
-            <div className="flex-wrap w-max sm:h-28 my-5 drop-shadow-2xl rounded-lg flex gap-y-4">
+        <section id="search-bar" className="h-max w-full flex justify-center">
+            <div className="flex-wrap w-max sm:h-28 my-5 drop-shadow-2xl rounded-lg flex justify-center items-center gap-y-4">
                 <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"date"}/>
                 <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700" type={"time"}/>
                 <Button className="w-40 md:w-56 h-10 rounded px-1" variant="contained">Search</Button>
