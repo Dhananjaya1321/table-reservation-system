52c7e713597db09494436abaddd5c2722a40b017
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-20 23:39:20 +0530

    Created SignIn component

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,18 @@ import {Home} from "./views/home";
 import {SearchBar} from "./views/searchBar";
 import {NavBar} from "./views/navBar/NavBar";
 import {Tables} from "./views/tables/Tables";
+import {SignIn} from "./views/SignIn/SignIn";
 
 function App() {
     return (
         <div className="App">
-            <NavBar/>
+           {/* <NavBar/>
             <main className="relative top-[50px] bg-gray-100">
                 <Home/>
                 <SearchBar/>
                 <Tables/>
-            </main>
+            </main>*/}
+            <SignIn/>
         </div>
     );
 }
