1e640b36e3a558b6f851d1c1f05010f61339f254
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-24 23:13:41 +0530

    Updated Footer component

--- src/views/Footer/Footer.tsx
+++ src/views/Footer/Footer.tsx
@@ -3,24 +3,24 @@ import fevIcon from "../../fevicon.png";
 export function Footer() {
     return (
         <footer className="mt-[50px] w-full h-max flex flex-col bg-gray-600">
-            <div className="text-white flex flex-row flex-wrap justify-around items-start my-10">
-                <img src={fevIcon}/>
-                <div className="flex flex-col">
-                    <menu>
+            <div className="text-white flex gap-5 flex-col sm:flex-row flex-wrap justify-center sm:justify-around items-center sm:items-start my-10">
+                <img className="hidden sm:block" src={fevIcon}/>
+                <div className="w-full sm:w-max flex flex-col justify-center items-center">
+                    <menu className="w-[70%]">
                         <li className="mb-3 text-2xl">LEGAL</li>
                         <li><a href="#">Privacy policy</a></li>
                         <li><a href="#">Terms & conditions</a></li>
                     </menu>
                 </div>
-                <div className="flex flex-col">
-                    <menu>
+                <div className="w-full sm:w-max flex flex-col justify-center items-center">
+                    <menu className="w-[70%]">
                         <li className="mb-3 text-2xl">FOLLOW US</li>
                         <li><a href="#">GitHub</a></li>
                         <li><a href="#">Linkedin</a></li>
                     </menu>
                 </div>
-                <div className="flex flex-col">
-                    <menu>
+                <div className="w-full sm:w-max flex flex-col justify-center items-center">
+                    <menu className="w-[70%]">
                         <li className="mb-3 text-2xl">LINKS</li>
                         <li><a href="#">Home</a></li>
                         <li><a href="#">About</a></li>
