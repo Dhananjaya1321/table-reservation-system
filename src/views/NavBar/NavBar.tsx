d42a878a69eede4baaebc5c0a2c3cf9864950619
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-24 23:26:50 +0530

    Updated NavBar component

--- src/views/NavBar/NavBar.tsx
+++ src/views/NavBar/NavBar.tsx
@@ -5,16 +5,16 @@ import {Link} from "react-router-dom";
 export class NavBar extends Component {
     render() {
         return (
-            <header className="fixed top-0 w-full h-[50px] z-50 flex justify-between bg-[#404040]">
-                <div className="w-full h-full flex justify-start">
+            <header className="fixed top-0 w-full h-[50px] z-50 flex md:justify-between bg-[#404040]">
+                <div className="w-max h-full flex justify-start">
                     <img src={logo} className="w-[50px] h-[50px] self-start"/>
                 </div>
                 <div className="w-full h-full flex items-center justify-end">
                     <menu className="">
-                        <li className="inline mx-5 text-white">Home</li>
-                        <li className="inline mx-5 text-white">About</li>
-                        <li className="inline mx-5 text-white">Tables</li>
-                        <li className="inline mx-5 text-white">Contact</li>
+                        <li className="hidden sm:inline mx-5 text-white">Home</li>
+                        <li className="hidden sm:inline mx-5 text-white">About</li>
+                        <li className="hidden sm:inline mx-5 text-white">Tables</li>
+                        <li className="hidden sm:inline mx-5 text-white">Contact</li>
                         <li className="inline mx-5 text-white">
                             <Button
                                 variant="contained"
