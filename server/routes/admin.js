c41f539043fcfa9ab96670a1b4c21511499dfe17
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-01-02 00:07:04 +0530

    Created post,put,delete and get methods for booking in admin.js file

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,11 +1,34 @@
 let express = require("express");
 let router = express.Router();
 
+/*================================= tables ==================================*/
 router.post('/table',function (req, res) {
     let table_id=req.body.id;
     let table_number=req.body.table_number;
     console.log(table_id,table_number);
     res.send(`ok`);
 });
+
+router.get('/all/table',function (req, res) {
+    res.send(`all tables`);
+});
+
+/*================================= booking ==================================*/
+router.post('/booking',function (req, res) {
+    res.send(`save booking`);
+});
+
+router.put('/booking',function (req, res) {
+    res.send(`update booking`);
+});
+
+router.delete('/booking',function (req, res) {
+    res.send(`delete booking`);
+});
+
+router.get('/new/booking',function (req, res) {
+    res.send(`new booking`);
+});
+
 
 module.exports=router
