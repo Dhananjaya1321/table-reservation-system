f3561ea1f9f9aad150efab2acf7620558062abd2
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-01-03 22:17:48 +0530

    Updated users.js file in server

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/*============================== save user ==============================*/
+router.post("/save/user",function (req, res) {
+    res.send("save user");
+})
+
+/*============================== get user count ==============================*/
+router.get("/user/count",function (req, res) {
+    res.send("users count");
+})
+
+/*============================== search user ==============================*/
+router.get("/search/user",function (req, res) {
+    res.send("search user");
+})
 
 module.exports = router;
