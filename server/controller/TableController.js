268cbb67215260f94feeeba2e17cc43dd9635f49
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-01-08 18:44:48 +0530

    Created saveTable function in TableController

--- server/controller/TableController.js
+++ server/controller/TableController.js
@@ -1,9 +1,21 @@
 const Table = require('../model/TableModel');
 
 const TableController = {
-    getAllTables:async function (req, res) {
+    getAllTables: async function (req, res) {
         try {
-            const tables =await Table.find();
+            const tables = await Table.find();
+            res.status(200).json(tables);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({
+                error: "Something Went Wrong!"
+            });
+        }
+    },
+
+    saveTable: async function (req, res) {
+        try {
+            const tables = await Table.create(req.body);
             res.status(200).json(tables);
         } catch (error) {
             console.error(error);
@@ -14,4 +26,4 @@ const TableController = {
     }
 }
 
-module.exports=TableController;
+module.exports = TableController;
