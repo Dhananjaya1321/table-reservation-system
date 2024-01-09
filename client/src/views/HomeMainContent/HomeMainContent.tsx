7dcac07e71aba83a789d7084d598d959a2386d78
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-01-09 14:16:12 +0530

    Updated Table component

--- client/src/views/HomeMainContent/HomeMainContent.tsx
+++ client/src/views/HomeMainContent/HomeMainContent.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import {NavBar} from "../NavBar/NavBar";
 import {Home} from "../Home/Home";
-import {SearchBar} from "../SearchBar/SearchBar";
 import {Tables} from "../Tables/Tables";
 import {About} from "../About/About";
 import {Feedback} from "../Feedback/Feedback";
@@ -14,7 +13,6 @@ export function HomeMainContent() {
             <NavBar/>
             <main className="relative top-[50px] bg-gray-100">
                 <Home/>
-                <SearchBar/>
                 <Tables/>
                 <About/>
                 <Feedback/>
--- client/src/views/Tables/Tables.tsx
+++ client/src/views/Tables/Tables.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from "react";
 import {Table} from "../Table/Table";
 import {api} from "../../config/config";
+import Button from "@mui/material/Button";
+import "./style.css";
 
 export function Tables() {
     let [tables, setTables] = useState([]);
@@ -28,18 +30,39 @@ export function Tables() {
         fetchData();
     }, []);
     return (
-        <section id="tables-section" className="w-full flex justify-center flex-col items-center">
-            <div className="w-[80%] h-[30px] flex flex-row flex-wrap gap-[25px]">
-                <div className="flex flex-row gap-[5px] items-center"><div className="w-[25px] h-[25px] bg-emerald-400 rounded-[5px] shadow-xl"/><p>Available</p></div>
-                <div className="flex flex-row gap-[5px] items-center"><div className="w-[25px] h-[25px] bg-red-400 rounded-[5px] shadow-xl"/><p>Not Available</p></div>
-            </div>
-            <section className="w-[80%] h-max flex flex-wrap flex-row gap-5 py-3 justify-center">
-                {
-                    tables.map((table:any)=>(
-                        <Table table_id={table.table_id} table_number={table.table_number} chair_count={4} status={""}/>
-                    ))
-                }
+        <>
+            <section id="search-bar" className="h-max w-full flex justify-center flex-row items-center flex-wrap">
+                <h1 className="mt-5 text-3xl mx-10 sm:mx-20 text-center">Receive Your Table Easily</h1>
+                <p className="text-center mx-10 sm:mx-20">To book the table, you need to move your cursor over the table
+                    or click. Select your desired date, time and check available tables before selecting your table</p>
+                <div
+                    className="flex-wrap w-max sm:h-28 my-5 drop-shadow-2xl rounded-lg flex justify-center items-center gap-y-4">
+                    <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700"
+                           type={"date"}/>
+                    <input className="w-40 md:w-56 h-10 rounded px-1 border border-solid border-slate-700"
+                           type={"time"}/>
+                    <Button className="w-40 md:w-56 h-10 rounded px-1" variant="contained">Search</Button>
+                </div>
             </section>
-        </section>
+            <section id="tables-section" className="w-full flex justify-center flex-col items-center">
+                <div className="w-[80%] h-[30px] flex flex-row flex-wrap gap-[25px]">
+                    <div className="flex flex-row gap-[5px] items-center">
+                        <div className="w-[25px] h-[25px] bg-emerald-400 rounded-[5px] shadow-xl"/>
+                        <p>Available</p></div>
+                    <div className="flex flex-row gap-[5px] items-center">
+                        <div className="w-[25px] h-[25px] bg-red-400 rounded-[5px] shadow-xl"/>
+                        <p>Not Available</p></div>
+                </div>
+                <section className="w-[80%] h-max flex flex-wrap flex-row gap-5 py-3 justify-center">
+                    {
+                        tables.map((table: any) => (
+                            <Table table_id={table.table_id} table_number={table.table_number} chair_count={4}
+                                   status={""}/>
+                        ))
+                    }
+                </section>
+            </section>
+        </>
+
     );
 }
